200bb82638930b0c5d05d47bde149782aafd38cf
theefactor4 <support@adbgames.net>
2025-01-22 15:06:00 +0000

    Update not-found.component.ts

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -13,7 +13,7 @@ import { FooterOneComponent } from '../../shared/layout/footer/footer-one/footer
     FooterOneComponent,
   ],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.css'
 })
 export class NotFoundComponent {
 
