b6fa3d1ff8b7759b1b5103950af7addb4f904e5a
theefactor4 <support@adbgames.net>
2025-01-22 15:20:20 +0000

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,27 +25,7 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 export const routes: Routes = [
     {path:"",redirectTo:"/home/home-one",pathMatch:"full"},
     {path:'home',children:[
-        {path:'home-one',component:HomeOneComponent,title:"ADB Games - Mykd"},
-        {path:'home-two',component:HomeTwoComponent,title:"Home Two - Mykd"},
-        {path:'home-three',component:HomeThreeComponent,title:"Home Three - Mykd"},
-        {path:'home-four',component:HomeFourComponent,title:"Home Four - Mykd"},
-        {path:'home-five',component:HomeFiveComponent,title:"Home Five - Mykd"},
+        
     ]},
-    {path:'tournament',component:TournamentComponent,title:"TournamentSkibidi - Mykd"},
-    {path:'tournament-details',component:TournamentDetailsComponent,title:"Tournament SingleSkibidi - Mykd"},
-    {path:'services',component:ServicesComponent,title:"Services - Mykd"},
-    {path:'service-details',component:ServiceDetailsComponent,title:"Services Single - Mykd"},
-    {path:'team-details/:id',component:TeamDetailsComponent,title:"Team Single - Mykd"},
-    {path:'login',component:LoginComponent,title:"Login - Mykd"},
-    {path:'register',component:RegisterComponent,title:"Register - Mykd"},
-    {path:'shop',component:ShopComponent,title:"Shop - Mykd"},
-    {path:'cart',component:CartComponent,title:"Cart - Mykd"},
-    {path:'checkout',component:CheckoutComponent,title:"Checkout - Mykd"},
-    {path:'shop-details/:id',component:ShopDetailsComponent,title:"Shop Single - Mykd"},
-    {path:'reset-password',component:ResetPasswordComponent,title:"Reset Password - Mykd"},
-    {path:'about',component:AboutComponent,title:"About - Mykd"},
-    {path:'blog',component:BlogComponent,title:"Blog - Mykd"},
-    {path:'blog-details/:id',component:BlogDetailsComponent,title:'Blog Details - Mykd'},
-    {path:'contact',component:ContactComponent,title:"Contact - Mykd"},
-    {path:'**',component:NotFoundComponent,title:"404 - Mykd"},
+    
 ];
